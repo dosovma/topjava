0c212d0ef63924a298d2c7dd0105e01a81db6cf2
dosovma <dosovma@mail.ru>
2020-10-20 23:28:27 +0300

    Do HW03 basic

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +10,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories)
+VALUES (100000, timestamp '2020-10-19 10:00', 'Завтрак', 500),
+       (100000, timestamp '2020-10-19 13:00', 'Обед', 1000),
+       (100000, timestamp '2020-10-19 20:00', 'Ужин', 500),
+       (100000, timestamp '2020-10-20 00:00', 'Еда на граничное значение', 100),
+       (100000, timestamp '2020-10-20 10:00', 'Завтрак', 1000),
+       (100000, timestamp '2020-10-20 13:00', 'Обед', 500),
+       (100000, timestamp '2020-10-20 20:00', 'Обед', 500);
